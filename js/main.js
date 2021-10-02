9f043bda74ecf2165a7127d761e82452262f73e5
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-10-02 12:49:48 +0200

    Realización de pantalla 1 y 2
    Realizamos la pantalla 1 y la selección de personajes.

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//AUTHOR : Alfonso Lara
+//version 1.0
+
+class gameHourse{
+    constructor(nombre,life,speed){
+    this.nombre = nombre;
+    this.life = life;
+    this.speed = speed;
+    this.distance= 0;
+    }
+//metodos    
+    getDistance(metros) {
+        this.distance += metros;
+
+    }
+}
+
+let hourse1 = new gameHourse ("Rayo",100,500);
+let hourse2 = new gameHourse ("Veloz",200,600);
+let hourse3 = new gameHourse ("Pecoso",150,550);
+let hourse4 = new gameHourse ("Happy",200,575);
+
+let players = [];
+
+//traducdor
+let traductor = {
+"1": hourse1,
+"2": hourse2,
+"3": hourse3,
+"4": hourse4
+}
+
+
+//Crear una funcionalidad al boton cuando pinchamos en el caballo lo meta en la variable uno.
+
+const chooseHourse = (caballo) => {
+    
+  
+  if(players.length <2){
+    players.push(traductor[caballo]);
+  }
+ console.log(players[0].nombre);
+ 
+  if(players.length ==2){
+    document.getElementById("spartida").style.display = "block";
+}
+  }
+
+
+
+
+
+
+
+
+organizer = (caballo) => {
+    let fasewant = "fase" + caballo;
+    
+    let arrFases = ["fase1", "fase2", "fase3", "fase4"];
+    arrFases = arrFases.filter(val => !caballo.includes(val));
+    
+    document.getElementById(caballo).style.display = "block";
+    
+    for (let _f of arrFases) {
+        document.getElementById(_f).style.display = "none";
+        console.log();
+    }
+}
