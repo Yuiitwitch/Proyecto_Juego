993605ac84c72ba36ff8f2c0f580c4108066da89
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-10-17 20:54:27 +0200

    finalización de la fase de la partida y cambio de estilos

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ let hourse4 = new gameHourse("Happy", 100, 30);
 let player1 = "";
 let player2 = "";
 
-let carrera = 1000;
+let metros = 1000;
 let partida = "";
 let ganador = "";
 
@@ -64,11 +64,12 @@ const hplayers = () => {
   organizer("3");
   document.getElementById("race").innerHTML =
     `
+
   <div><img class="player1" src="../img/${player1.nombre}.jpg" id="ph1"/>
-  <div class="p1t">Player1</div>
+  <div>${player1.nombre}</div>
 </div>
-<div><img class="player2" src="img/${player2.nombre}.jpg" id="ph2"/>
-  <div class="p2t">Player2</div>
+<div><img class="player2" src="../img/${player2.nombre}.jpg" id="ph2"/>
+  <div class="p2t">${player2.nombre}</div>
 </div>
 <div class="distance">
   <div id="distanceh1">Mts:${player1.distance}</div>
@@ -79,50 +80,92 @@ const hplayers = () => {
 <div class="boton1" id="carrera" onclick="acelerar()"><button>GO!</button></div> 
   `;
 }
-// juego
+  // juego
 
-const acelerar1 = () => {
-  let random = Math.floor(Math.random() * (100 - 10) + 10);
-  player1.distance = parseInt((random + player1.distance))
+  const acelerar1 = () => {
+    let random = Math.floor(Math.random() * (100 - 10) + 10);
+    player1.distance = parseInt((random + player1.distance))
 
-  console.log(player1.distance);
-  document.getElementById("distanceh1").style.botton = `${player1.distance}`
-}
-const acelerar2 = () => {
-
-  let random = Math.floor(Math.random() * (100 - 10) + 10);
-  player2.distance = parseInt((random + player2.distance))
-
-  console.log(player2.distance);
-  document.getElementById("distanceh2").style.botton = `${player2.distance}`
-}
-const acelerar = () => {
-  acelerar1();
-  acelerar2();
-}
-
-const comprar = () => {
-  
-  if()
-}
-
-
-
-
-
-
-
-
-organizer = (num) => {
-  let fasewant = "fase" + num;
-
-  let arrFases = ["fase1", "fase2", "fase3", "fase4"];
-  arrFases = arrFases.filter(val => !fasewant.includes(val));
-
-  document.getElementById(fasewant).style.display = "block";
-
-  for (let _f of arrFases) {
-    document.getElementById(_f).style.display = "none";
-    console.log();
+    console.log(player1.distance);
+    document.getElementById("carrera").style.botton= `${player1.distance}`
+    document.getElementById("distanceh1").innerHTML = `Metros recorridos P1: ${player1.distance}`;
   }
-}
+  const acelerar2 = () => {
+
+    let random = Math.floor(Math.random() * (100 - 10) + 10);
+    player2.distance = parseInt((random + player2.distance))
+
+    console.log(player2.distance);
+    document.getElementById("carrera").style.botton = `${player2.distance}`
+    document.getElementById("distanceh2").innerHTML = `Metros recorridos P2: ${player2.distance}`;
+  }
+  const acelerar = () => {
+    acelerar1();
+    acelerar2();
+    comparar();
+  }
+  //funcion que compara la distance que lleva cada caballo para determinar el ganador
+  const comparar = () => {
+
+    if (player1.distance < metros && player2.distance < metros) {
+
+      console.log("continua la carrera");
+    } else {
+      document.getElementById("carrera").style.display = "none";
+      if (player1.distance > player2.distance && player2.distance < player1.distance) {
+        console.log(`THE WINNER IS ${player1.nombre}`);
+
+      } else {
+        console.log(`El ganaor es ${player2.nombre}`)
+
+        setTimeout(() => {
+
+          organizer("4");
+          acelerar();
+          final();
+
+        }, 1000);
+
+        ganador.innerHTML = `El ganador es ${player2.nombre} !!!!`;
+
+      }
+    }
+  }
+
+
+  //funcion final donde determinamos el ganador.
+
+  const final = () => {
+    document.getElementById("campeon").innerHTML = 
+    `
+    <div class="winner">
+        THE WINNER IS:
+    <div>${ganador.nombre}</div>    
+    </div>
+    <div class="hwinner">
+    </div>
+    <div>
+        <button id="restart" type="button" onclick='("1")'>RESTART</button>
+    </div>
+    `;
+  }
+
+
+
+
+
+
+
+  organizer = (num) => {
+    let fasewant = "fase" + num;
+
+    let arrFases = ["fase1", "fase2", "fase3", "fase4"];
+    arrFases = arrFases.filter(val => !fasewant.includes(val));
+
+    document.getElementById(fasewant).style.display = "block";
+
+    for (let _f of arrFases) {
+      document.getElementById(_f).style.display = "none";
+      console.log();
+    }
+  }
