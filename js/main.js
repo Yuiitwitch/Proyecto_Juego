630c348caf47d3c4ea684eb5430be067dd7271dd
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-10-17 17:07:18 +0200

    Prueba 2 de inicio de partida

--- js/main.js
+++ js/main.js
@@ -1,86 +1,128 @@
 //AUTHOR : Alfonso Lara
 //version 1.0
 
-class gameHourse{
-    constructor(nombre,life,speed){
+class gameHourse {
+  constructor(nombre, aceleracion) {
     this.nombre = nombre;
-    this.life = life;
-    this.speed = speed;
-    this.distance= 0;
-    }
-//metodos    
-    setDistance(metros) {
-        this.distance += metros;
+    this.aceleracion = aceleracion;
+    this.distance = 0;
+  }
+  //metodos    
+  setDistance(metros) {
+    this.distance += metros;
 
-    }
-    getDistance(){
-    }
+  }
+  getDistance() {
+  }
 }
 
-let horse1 = new gameHourse ("Rayo",100,30);
-let horse2 = new gameHourse ("Veloz",100,30);
-let horse3 = new gameHourse ("Pecoso",100,30);
-let horse4 = new gameHourse ("Happy",100,30);
+let hourse1 = new gameHourse("Rayo", 100, 30);
+let hourse2 = new gameHourse("Veloz", 100, 30);
+let hourse3 = new gameHourse("Pecoso", 100, 30);
+let hourse4 = new gameHourse("Happy", 100, 30);
 
 let players = [];
 
 //traducdor
 let traductor = {
-"1": horse1,
-"2": horse2,
-"3": horse3,
-"4": horse4
+  "rayo": hourse1,
+  "veloz": hourse2,
+  "pecoso": hourse3,
+  "happy": hourse4
 }
 
 
 //Crear una funcionalidad al boton cuando pinchamos en el caballo lo meta en la variable uno.
 
-let arrayHourse = [];
-const chooseHourse = (caballo) => {
-    
-  arrayHourse[i] = caballo;
-  document.getElementById("spartida" + i).style.display = "block";
-  indice++;
-  go();
-  }
+const chooseHorse = (caballo) => {
 
+
+  for (let index = 0; index < 2; index++) {
+    players.push(traductor[caballo]);
+    console.log(caballo);
+    document.querySelector('#' + caballo).classList.add("trans")
+  }
+  // if (players == null{
+  //   players = traductor[caballo]
+  // }
+
+
+  if (players.length > 3) {
+    document.getElementById("spartida").style.display = "block";
+  }
+  if(player1 != ""){
+    players = traductor[caballo];
+    chooseHorse()
+
+    setTimeout(() =>{
+        organizer("3")
+        jugadores();
+
+    }, 1000);
+  }
+}
+
+//funcion mostrar jugadores
+const hplayers = () => {
+  document.getElementById("race").innerHTML =
+    `
+  <div><img class="player1" src="img/${players.nombre}.jpg" id="ph1"/>
+  <div class="p1t">Player1</div>
+</div>
+<div><img class="player2" src="img/${players.nombre}.jpg" id="ph2"/>
+  <div class="p2t">Player2</div>
+</div>
+<div class="distance">
+  <div id="distanceh1">Mts:${players.distance}</div>
+  <div id="distanceh2">Mts:${players.distance}</div>
+</div>
+</div>
+</div>
+<div class="boton1" id="carrera" onclick="acelerar()"><button>GO!</button></div> 
+  `;
+}
 //juego//
 
 
 let carrera = 1000;
-let player1 = 0;
-let player2 = 0;
+let partida = "";
+let player1 = "";
+let player2 = "";
 
+const acelerar1 = () => {
 
+  let random = Math.floor(Math.random() * (25 - 10) + 10);
+  player1.distancia += parseInt((random * player1.distancia))
 
-const go = () =>{
-  if (i==2) {
+  console.log(player1.distancia);
+  document.getElementById("distanceh1").style.botton = player1.distancia / 20 + "em";
+}
+const acelerar2 = () => {
 
-    organizer("fase4");
+  let random = Math.floor(Math.random() * (25 - 10) + 10);
+  player2.distancia += parseInt((random * player2.distancia))
 
-    player1 += Math.random() * (100 - 50) + 50;
-    player2 += Math.random() * (100 - 50) + 50;
-    
-    if (player1 >= carrera || player2 >= carrera) {
-      document.getElementById("carrera").style.display ="block";
-      clearInterval(carrera);
-    }
-    if (player1>player2) {
-      document.getElementById("")
-    }
-  }
+  console.log(player2.distancia);
+  document.getElementById("distanceh2").style.botton = player2.distancia / 20 + "em";
 }
 
+
+
+
+
+
+
+
 organizer = (caballo) => {
-    let fasewant = "fase" + caballo;
-    
-    let arrFases = ["fase1", "fase2", "fase3", "fase4"];
-    arrFases = arrFases.filter(val => !caballo.includes(val));
-    
-    document.getElementById(caballo).style.display = "block";
-    
-    for (let _f of arrFases) {
-        document.getElementById(_f).style.display = "none";
-        console.log();
-    }
+  let fasewant = "fase" + caballo;
+
+  let arrFases = ["fase1", "fase2", "fase3", "fase4"];
+  arrFases = arrFases.filter(val => !caballo.includes(val));
+
+  document.getElementById(caballo).style.display = "block";
+
+  for (let _f of arrFases) {
+    document.getElementById(_f).style.display = "none";
+    console.log();
+  }
 }
